c2f6e1ff1e47706dd33e2e1ed001671746d3df73
Mustfdemrc <mustf.dmrc346@gmail.com>
2025-05-21 14:54:56 +0300

    Add files via upload

---
+++ AskerlikKontrol/AskerlikKontrol/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AskerlikKontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Askerlik Yaptığınız Gün:";
+            button1.Text = "Hesapla";
+            label2.Text = "Sonuç";            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int gun = Convert.ToInt32(textBox1.Text);
+
+            if (gun == 180)
+            {
+                label2.Text = "Askerliğiniz Bitti Tezkereniz Hayırlı Olsun";
+            }
+            else if (gun < 180)
+            {
+                label2.Text = "Askerliğnizin Bitmesine " + Convert.ToString(180 - gun) + " Gün Kalmıştır.";
+            }
+            else
+            {
+                label2.Text = "Fazla Gün Girilmiştir.";
+            }
+        }
+
+    }
+}
---
+++ AskerlikKontrol/AskerlikKontrol/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("AskerlikKontrol")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AskerlikKontrol")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("8d90c3e9-9a07-4624-ba97-64a1e1cab9a2")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
